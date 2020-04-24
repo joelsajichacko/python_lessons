9cd23317c795694dae4daa9692aace010aa5da69
JOEL SAJI CHACKO <joelsajichacko@gmail.com>
2020-04-25 00:04:45 +0530

    updated

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -25,7 +25,7 @@ print('Pick a hand: (0: Rock, 1: Paper, 2: Scissors)')
 player_hand = int(input('Please enter a number (0-2): '))
 
 if utils.validate(player_hand):
-    computer_hand = random.randint(0,2)
+    computer_hand = random.randint(0,2) # random choice by computer
     
     utils.print_hand(player_hand, player_name)
     utils.print_hand(computer_hand, 'Computer')
